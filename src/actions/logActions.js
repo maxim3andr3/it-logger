7ea64dd915d94430e6628af7ce8036c12fb4ca93
maxim3andr3 <maxim3andr3@gmail.com>
2019-12-11 15:31:14 +0100

    Update Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -2,6 +2,9 @@ import {
   GET_LOGS,
   ADD_LOG,
   DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
   SET_LOADING,
   LOGS_ERROR,
 } from './types';
@@ -85,6 +88,48 @@ export const deleteLog = id => async dispatch => {
       payload: err.response.data,
     });
   }
+};
+
+// Update log on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Set current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+// Clear current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT,
+  };
 };
 
 // Set loading to true
